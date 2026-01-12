a00f4ede7eea833d381841564d022b0180f78c41
yanglei19950302 <yanglei520@h-partners.com>
2026-01-12 19:20:55 +0800

    微重构dlopen框架，dlopen打开失败时，抛出异常，终止程序运行

--- virtrust/src/virtrust/dllib/common.h
+++ virtrust/src/virtrust/dllib/common.h
@@ -99,16 +99,12 @@ protected:
         VIRTRUST_ENFORCE(libptr_ != nullptr, std::string(dlerror()));
     }
 
-    template <class R, class... Args> DllibRc SelfDlSym(std::string_view funName, DlFun<R, Args...> &outFun)
+    template <class R, class... Args> void SelfDlSym(std::string_view funName, DlFun<R, Args...> &outFun)
     {
-        if (libptr_ == nullptr || funName.empty()) {
-            return DllibRc::ERROR;
-        }
-
+        VIRTRUST_ENFORCE(libptr_ != nullptr && !funName.empty(), "funName is empty or libprt is nullptr");
         void *funPtr = dlsym(libptr_, funName.data());
         size_++; // always add up internal size counter
         outFun = DlFun<R, Args...>(funName, reinterpret_cast<R (*)(Args...)>(funPtr));
-        return DllibRc::OK;
     }
 
 private:
